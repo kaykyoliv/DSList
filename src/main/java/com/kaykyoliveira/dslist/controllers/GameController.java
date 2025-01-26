2ada9d467ebf97ea7614b2fdb6d11295f5605d00
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-01-26 20:26:16 -0300

    GET game by id, Database seed update

--- src/main/java/com/kaykyoliveira/dslist/controllers/GameController.java
+++ src/main/java/com/kaykyoliveira/dslist/controllers/GameController.java
@@ -1,5 +1,6 @@
 package com.kaykyoliveira.dslist.controllers;
 
+import com.kaykyoliveira.dslist.dto.GameDTO;
 import com.kaykyoliveira.dslist.dto.GameMinDTO;
 import com.kaykyoliveira.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,12 @@ public class GameController {
 
     @Autowired
     private GameService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<GameDTO> findById(@PathVariable Long id){
+        GameDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
 
     @GetMapping
     public ResponseEntity<Page<GameMinDTO>> findAll(Pageable pageable){
--- src/main/java/com/kaykyoliveira/dslist/services/GameService.java
+++ src/main/java/com/kaykyoliveira/dslist/services/GameService.java
@@ -1,5 +1,6 @@
 package com.kaykyoliveira.dslist.services;
 
+import com.kaykyoliveira.dslist.dto.GameDTO;
 import com.kaykyoliveira.dslist.dto.GameMinDTO;
 import com.kaykyoliveira.dslist.entities.Game;
 import com.kaykyoliveira.dslist.repositories.GameRepository;
@@ -14,9 +15,17 @@ public class GameService {
 
     @Autowired
     private GameRepository repository;
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game entity = repository.findById(id).get();
+        return new GameDTO(entity);
+    }
+
     @Transactional(readOnly = true)
     public Page<GameMinDTO> findAll(Pageable pageable){
         Page<Game> result = repository.findAll(pageable);
         return result.map(x -> new GameMinDTO(x));
     }
+
 }
