33210b874d49b5e2a9ee78f496f945e3ef058ef3
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-01-27 11:30:57 -0300

    Find game by list

--- src/main/java/com/kaykyoliveira/dslist/controllers/GameListController.java
+++ src/main/java/com/kaykyoliveira/dslist/controllers/GameListController.java
@@ -1,14 +1,19 @@
 package com.kaykyoliveira.dslist.controllers;
 
 import com.kaykyoliveira.dslist.dto.GameListDTO;
+import com.kaykyoliveira.dslist.dto.GameMinDTO;
 import com.kaykyoliveira.dslist.services.GameListService;
+import com.kaykyoliveira.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -16,10 +21,19 @@ public class GameListController {
 
     @Autowired
     private GameListService service;
-    
+
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
     public ResponseEntity<Page<GameListDTO>> findAll(Pageable pageable){
         Page<GameListDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(value = "/{listId}/games")
+    public ResponseEntity<List<GameMinDTO>> findByList(@PathVariable Long listId){
+        List<GameMinDTO> dto = gameService.findByList(listId);
         return ResponseEntity.ok(dto);
     }
 }
--- src/main/java/com/kaykyoliveira/dslist/services/GameService.java
+++ src/main/java/com/kaykyoliveira/dslist/services/GameService.java
@@ -3,12 +3,16 @@ package com.kaykyoliveira.dslist.services;
 import com.kaykyoliveira.dslist.dto.GameDTO;
 import com.kaykyoliveira.dslist.dto.GameMinDTO;
 import com.kaykyoliveira.dslist.entities.Game;
+import com.kaykyoliveira.dslist.projections.GameMinProjection;
 import com.kaykyoliveira.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GameService {
@@ -26,6 +30,11 @@ public class GameService {
     public Page<GameMinDTO> findAll(Pageable pageable){
         Page<Game> result = repository.findAll(pageable);
         return result.map(x -> new GameMinDTO(x));
+    }
+
+    public List<GameMinDTO> findByList(Long id){
+        List<GameMinProjection> list = repository.searchByList(id);
+        return list.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
     }
 
 }
